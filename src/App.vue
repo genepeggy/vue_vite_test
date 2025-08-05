52f97789c03b344a0cb5b933b22ebf36525e32fa
Peggy <peggy2024gene@gmail.com>
2025-08-05 17:34:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- js或ts start -->
+<script lang="ts">
+    export default{
+        name:'App', //元件名
+    }
+</script>
+<!-- js或ts end -->
+<!-- html結構 start -->
+<template>
+    <div class="app">
+        <h1>Hello World!</h1>
+    </div>
+</template>
+<!-- html結構 end -->
+<!-- 樣式 start -->
+<style scoped>
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
+<!-- 樣式 end -->
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// 引入createApp用於創建實例
+import {createApp} from 'vue'
+// 引入App根元件
+import App from './App.vue'
+
+createApp(App).mount('#app')
