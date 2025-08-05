a9ea37c1eeec7c72979149c3e908d9934e7e8205
Peggy <peggy2024gene@gmail.com>
2025-08-05 18:55:08 +0800

    📝 vue3 pratice

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
+<!-- Vue2 -->
+<!-- App.vue -->
 <!-- js或ts start -->
 <script lang="ts">
+    import Person from './components/Person.vue';
     export default{
         name:'App', //元件名
+        components:{
+            Person
+        }
     }
 </script>
 <!-- js或ts end -->
 <!-- html結構 start -->
 <template>
-    <div class="app">
-        <h1>Hello World!</h1>
-    </div>
+    
+        <!-- vue3可以有多個物件標籤，vue2則不可 -->
+        <!-- <Person></Person>
+        <Person></Person>
+        <Person></Person> -->
+        <Person></Person>
+    
 </template>
 <!-- html結構 end -->
 <!-- 樣式 start -->
