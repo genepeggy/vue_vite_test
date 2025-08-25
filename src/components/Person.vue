9fa3f9c25f0246d0572615686f906da95c726828
Peggy <peggy2024gene@gmail.com>
2025-08-25 14:37:28 +0800

    🎨 setup語法糖練習

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,64 +1,54 @@
 <!-- Vue2 -->
 <!-- Person.vue -->
+ 
 <!-- js或ts start -->
+
+<!-- 拿來設定component名字 -->
 <script lang="ts">
     export default{
+        name:"person",
+    }
+</script>
+<!-- 拿來設定compositionAPI -->
+<script lang='ts' setup>
+    //數據 
+    let name = "Peggy"
+    let age = 18
+    let tel = "013"
+    // 不需要寫return
 
-        setup(){
-            
-            // setup中不能寫this，this是undefined，在vue3中this被弱化了
-            // data
-            let name = "Peggy"
-            let age = 18
-            let tel = "013"
-            // let x = d
-            
-            // 方法
-            function changeName(){
-                
-                
-                name="Peggy2"
-                console.log(name);
-                
-            }
-            function changeAge(){
-                
-                
-                age=5
-                console.log(age);
-            }
-            function showTel(){
-                
-                
-                alert(tel)
-                console.log(tel);
-            }
-            return{
-                name:name,
-                age:age,
-                changeName,
-                changeAge,
-                showTel
-            }
-            // return返回值也可以是函式
-            // return () => 'haha'
-        }
+     // 方法
+    function changeName(){
+        name="Peggy2"
+        console.log(name);
+        
+    }
+    function changeAge(){
+        
+        
+        age=5
+        console.log(age);
+    }
+    function showTel(){
+        
+        
+        alert(tel)
+        console.log(tel);
     }
 </script>
 <!-- js或ts end -->
+
 <!-- html結構 start -->
 <template>
     <div class="person">
+        <!-- <h2>{{ a }}</h2> -->
         <h2>姓名:{{ name }}</h2>
         <h2>年齡:{{ age }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年齡</button>
         <button @click="showTel">顯示電話</button>
         <hr>
-        <h2>測試:{{a}}</h2>
-        <h2>測試:{{c}}</h2>
-        <h2>測試:{{d}}</h2>
-        <button @click="b">測試</button>
+
     </div>
 </template>
 <!-- html結構 end -->
